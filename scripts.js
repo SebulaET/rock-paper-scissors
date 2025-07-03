416813bdc49175055c096707d5e13905b8ee6d64
SebulaET <sebula.et@gmail.com>
2025-07-03 13:13:34 -0400

    Add some pseudocode to scripts
    It looks awful I have no idea what I'm doing.

---
+++ scripts.js
@@ -0,0 +1,11 @@
+// Generate random result between Rock, Paper, Scissors from computer
+
+// Get input from player for their choice
+
+// Return a value of Rock, Paper, or Scissors
+
+// Keep track of score
+
+// Combine to play one round
+
+// Combine to play whole game
