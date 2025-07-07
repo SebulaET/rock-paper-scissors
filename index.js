ce6d652e6a6be254d7361cde6451d57147f7a6bf
SebulaET <sebula.et@gmail.com>
2025-07-07 14:08:13 -0400

    Add functions playRound and checkWinner
    Add function to play out one round, add function
    to check winner of each round to get result.

--- index.js
+++ index.js
@@ -2,7 +2,25 @@ const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
-    console.log(choice);
+    return choice;
 }
 
-getComputerChoice();
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Tie";   
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playeSelection == "scissors" && computerSelection == "paper")
+    ){
+        return "You win! :)";
+    }
+    else {
+        return "You lose. :(";
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+
+}
