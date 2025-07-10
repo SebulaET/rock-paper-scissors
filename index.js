1820f31392899e27727a77fa976912d1c7230189
SebulaET <sebula.et@gmail.com>
2025-07-10 14:12:04 -0400

    Add play game function
    Add in a function to play five rounds and determine an
    overall winner. Moved variables inside, need to move things around.

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ function getPlayerChoice(){
 // Keep score between player and computer.
     // Tally the player score.
     // Tally the PC score.
-let playerScore = 0;
-let computerScore = 0;
+
 // Combine to play one round.
     // Take the player choice.
     // Take the PC choice.
@@ -31,22 +30,38 @@ function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
             console.log("It's a tie!");
     }
-    else if ((playerSelection == "rock" && computerSelection == "scissors") ||
-            (playerSelection == "paper" && computerSelection == "rock") ||
-            (playerSelection == "scissors" && computerSelection == "paper")){
-                console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
-                playerScore++;
+    else if
+        ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+            console.log(`You won! ${playerSelection} beats ${computerSelection}.`);
+            playerScore++;
     }
-    else{
+    else {
         console.log(`You lost... ${computerSelection} beats ${playerSelection}`);
         computerScore++;
     }
 }
-
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
-
-playRound(playerSelection, computerSelection);
 // Play whole game.
     // Repeat round of play five times.
-    // 
+    // keep score until winner
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+    const playerSelection = getPlayerChoice();
+    const computerSelection = getComputerChoice();
+
+    for (let i = 0; i < 5; i++) {
+        playRound(playerSelection, computerSelection);
+    }
+
+    if (playerScore > 2){
+        console.log("You won the game!");
+    }
+    else {
+        console.log("You lost the game :(");
+    }
+}
+
+playGame();
+// YOU ARE HERE Now the score and selection variables are in the game function. Refer to Hint on step 6 for what we're focusing on now.
